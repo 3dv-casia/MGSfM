456b2db8015f67a6a6ba0da75c55612d64475cf3
陶沛霖 <2663587961@qq.com>
2025-07-21 19:42:23 +0800

    Remove wrong comments

--- demo-kitti.sh
+++ demo-kitti.sh
@@ -6,12 +6,12 @@ for data in "KITTI01" "KITTI06"; do
     database_path="${data_dir}database.db"
 
     # multi-camera mapping
-    # build/glomap/mgsfm multi_mapper \
-    #     --database_path "$database_path" \
-    #     --output_path "${data_dir}mgsfm_result/" \
-    #     --image_path "" \
-    #     --ba_iteration_num 2 \
-    #     --skip_retriangulation 1
+    build/glomap/mgsfm multi_mapper \
+        --database_path "$database_path" \
+        --output_path "${data_dir}mgsfm_result/" \
+        --image_path "" \
+        --ba_iteration_num 2 \
+        --skip_retriangulation 1
 
     # visualize the results in COLMAP GUI:
     colmap gui --import_path "${data_dir}mgsfm_result/0/" --database_path "$database_path" --image_path "$data_dir"
